116dcc2d0613344f27fc9672b56027fc1a364a0d
skandavc18 <38657669+skandavc18@users.noreply.github.com>
2020-05-17 20:40:49 +0530

    Update python.sh

--- Phase 6/python.sh
+++ Phase 6/python.sh
@@ -1,40 +1,56 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
-    echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py [-option]"
+    echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py"
     exit 2
 fi
 make > /dev/null
 if [[ $# -eq 2 ]]; then
-    if [[ $1 == "-S" ]] || [[ $1 == "-s" ]]; then 
+    if [[ $1 == "-S" ]] || [[ $1 == "-s" ]] || [[ $1 == "-si1" ]] || [[ $1 == "-i1s" ]] || [[ $1 == "-Si1" ]] || [[ $1 == "-i1S" ]]; then 
         if test -f "$2"; then
             fbname=$(basename "$2" .py).S
             cat $2 | ./python.out | python opt1.py | python compile.py > $fbname
         else
-            echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py [-option]"
+            echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py"
+            make clean > /dev/null
+            exit 2
+        fi
+    elif [[ $1 == "-I1" ]] || [[ $1 == "-i1" ]]; then 
+        if test -f "$2"; then
+            fbname=$(basename "$2" .py).S
+            cat $2 | ./python.out | python opt1.py > $fbname
+        else
+            echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py"
+            make clean > /dev/null
+            exit 2
+        fi
+    elif [[ $1 == "-I0" ]] || [[ $1 == "-i0" ]]; then 
+        if test -f "$2"; then
+            fbname=$(basename "$2" .py).S
+            cat $2 | ./python.out > $fbname
+        else
+            echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py"
+            make clean > /dev/null
+            exit 2
+        fi
+    elif [[ $1 == "-SI0" ]] || [[ $1 == "-si0" ]] || [[ $1 == "-i0s" ]] || [[ $1 == "-i0S" ]]; then 
+        if test -f "$2"; then
+            fbname=$(basename "$2" .py).S
+            cat $2 | ./python.out | python compile.py > $fbname
+        else
+            echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py"
             make clean > /dev/null
             exit 2
         fi
     else
-        if [[ $1 != "-S" ]] || [[ $1 != "-s" ]]; then
-                echo -e "python: \033[1;31m error: \033[0m wrong option\nusage: ./python.sh [-option] <inputfile>.py [-option]"
-                make clean > /dev/null
-                exit 2
-        else
-            if test -f "$1"; then
-                fbname=$(basename "$1" .py).S
-                cat $1 | ./python.out | python opt1.py | python compile.py > $fbname
-            else
-                echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py [-option]"
-                make clean > /dev/null
-                exit 2
-            fi
-        fi  
+        echo -e "python: \033[1;31m error: \033[0m wrong option\nusage: ./python.sh [-option] <inputfile>.py"
+        make clean > /dev/null
+        exit 2 
     fi
 else
     if test -f "$1"; then
         cat $1 | ./python.out | python opt1.py | python compile.py
     else
-        echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py [-option]"
+        echo -e "python: \033[1;31m error: \033[0m no input file\nusage: ./python.sh [-option] <inputfile>.py"
         make clean > /dev/null
         exit 2
     fi
